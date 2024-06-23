ef0bb7c64525c3b61c58f60e8653fae863fc5162
Diego Fabricio <difagume@gmail.com>
2024-06-23 08:58:12 -0500

    junit results

--- vitest.config.js
+++ vitest.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
- 
+
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    environment: 'jsdom',
-  },
+	plugins: [react()],
+	test: {
+		environment: 'jsdom',
+		compilerOptions: {
+			types: ['vitest/jsdom']
+		},
+		reporters: ['junit'],
+		outputFile: './test-results/junit-report.xml'
+	}
 })
